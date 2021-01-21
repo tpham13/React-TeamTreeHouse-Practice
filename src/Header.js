f65d0897df284240702924ff05cec024a26ea350
tpham13 <tpham0123@gmail.com>
2021-01-20 23:18:56 -0800

    Create Header and App component then render Header inside App component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+//another way of writing components is using the arrow function instead of function Header()
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stat">Players:1</span>
+    </header>
+  );
+}
+
+
+
+export default Header;
